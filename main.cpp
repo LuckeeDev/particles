c17be8f92b33c5fe1c5a29b1aab6e6ea1bf88ecf
Luca Zoppetti <luckeedev@gmail.com>
2023-10-26 11:18:47 +0200

    add cases to test program

--- main.cpp
+++ main.cpp
@@ -1,10 +1,14 @@
 #include <iostream>
 #include <vector>
 
+#include "Particle.hpp"
 #include "ParticleType.hpp"
 #include "ResonanceType.hpp"
 
 int main() {
+  std::cout << "TESTING THE \"ParticleType\" AND \"ResonanceType\" CLASSES"
+            << '\n';
+
   ParticleType const pt{"pt test", 5.7, -1};
 
   std::cout << pt.getName() << ' ' << pt.getMass() << ' ' << pt.getCharge()
@@ -24,4 +28,25 @@ int main() {
   for (auto const& p : ptr_vector) {
     p->print();
   }
+
+  std::cout << "\n\n"
+            << "TESTING THE \"Particle\" CLASS" << '\n';
+
+  Particle::addParticleType("electron", 5.7, -1);
+  Particle::addParticleType("proton", 1.025, 10, 7);
+
+  Particle e{"electron"};
+  Particle p{"proton"};
+  Particle n{"neutron"};
+
+  e.printData();
+  p.printData();
+  n.printData();
+
+  Particle::addParticleType("electron", 1.025, 0, 7);
+  Particle::addParticleType("neutron", 1.025, 0, 7);
+  n.setIndex("neutron");
+  n.printData();
+
+  Particle::printParticleTypes();
 }
